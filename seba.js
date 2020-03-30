6ebff7d0af5fd389f59334531b28e4deafe7c7c1
sev@PlateSP4 <mtsev@users.noreply.github.com>
2020-03-30 15:00:03 +1100

    wip cron for late night channels

--- seba.js
+++ seba.js
@@ -1,14 +1,30 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const { token, welcome_ch, rules_ch, intro_ch } = require('./config.json');
+const CronJob = require('cron').CronJob;
+const id = require('./config.json');
 
+/* Ready message */
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
 
+/* Welcome message on user join */
 client.on('guildMemberAdd', member => {
-    var message = `Welcome to UNSW lo-fi society, ${member}! Please read the <#${rules_ch}> and introduce yourself in <#${intro_ch}>.`
-    client.channels.get(welcome_ch).send(message);
+    var message = `Welcome to UNSW lo-fi society, ${member}! Please read the <#${id.rules_ch}> and introduce yourself in <#${id.intro_ch}>.`
+    client.channels.get(id.welcome_ch).send(message);
 });
 
-client.login(token);
+/* Late night voice channel active 01:00 - 06:00 */
+const show_ln_chan = new CronJob('00 00 01 * * *', function() {
+    client.channels.get(id.ln_cat).overwritePermissions(id.everyone, {
+        'SEND_MESSAGES': true,
+    })
+});
+const hide_ln_chan = new CronJob('00 00 06 * * *', function() {
+	
+});
+
+
+client.login(id.token);
+show_ln_chan.start();
+hide_ln_chan.start()
