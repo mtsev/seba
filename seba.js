6740200b6d4631320f9f95f80c2d3abcaf3c05a2
sev@PlateSP4 <mtsev@users.noreply.github.com>
2020-04-10 04:08:31 +1000

    add late night channels feature to seba

--- seba.js
+++ seba.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
+const CronJob = require('cron').CronJob;
 const Discord = require('discord.js');
-const { prefix, token, server, channels } = require('./config.json');
+const { prefix, token, server, channels, roles } = require('./config.json');
 
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
@@ -12,9 +13,57 @@ for (const file of commandFiles) {
 	client.commands.set(command.name, command);
 }
 
-// Output to console on successful login
+// Initialisation sequence
+var guild;
 client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
+    let d = new Date();
+    console.log(`[${d.toLocaleString()}] Logged in as ${client.user.tag}!`);
+    guild = client.guilds.get(server.id);
+
+    // Start late night cron jobs
+    showLateNights.start();
+    hideLateNights.start();
+});
+
+
+/* Late night channels active 01:00 - 06:00 */
+const showLateNights = new CronJob('00 00 01 * * *', async () => {
+    const lateNights = await guild.channels.get(categories.lateNights);
+    let d = new Date();
+
+    // Move category into position
+    await lateNights.setPosition(2)
+                .then(newChannel => console.log(`${lateNights.name}'s new position is ${newChannel.position}`))
+                .catch(console.error);
+
+    // Give access to members
+    await lateNights.overwritePermissions(guild.roles.get(roles.verified), {
+        VIEW_CHANNEL: true,
+        SEND_MESSAGES: true,
+        CONNECT: true
+    })
+        .then(() => console.log(`[${d.toLocaleString()}] Late nights hours are now in session`))
+        .catch(console.error);
+});
+
+const hideLateNights = new CronJob('00 00 06 * * *', async () => {
+    const lateNights = await guild.channels.get(categories.lateNights);
+    const position = await guild.channels.get(categories.exec).position + 1;
+    let d = new Date();
+
+    // Move category back down
+    await lateNights.setPosition(position)
+                .then(newChannel => console.log(`${lateNights.name}'s new position is ${newChannel.position}`))
+                .catch(console.error);
+
+    // Remove access from members
+	await lateNights.overwritePermissions(guild.roles.get(roles.verified), {
+        VIEW_CHANNEL: false,
+        SEND_MESSAGES: false,
+        CONNECT: false
+    })
+        .then(() => console.log(`[${d.toLocaleString()}] Rise with the moon go to bed with the sun`))
+        .catch(console.error);
 });
 
 
@@ -28,20 +77,34 @@ client.on('guildMemberAdd', async member => {
 
 // Bot commands
 client.on('message', async message => {
+
+    // Ignore non-commands and messages from bots
     if (!message.content.startsWith(prefix) || message.author.bot) return;
 
+    // Parse message
     const args = message.content.slice(prefix.length).split(/ +/);
     const commandName = args.shift().toLowerCase();
-
-    if (!client.commands.has(commandName)) return;
-
     const command = client.commands.get(commandName);
 
+    // Ignore invalid command
+    if (!command) return;
+
+    // Check for exec only commmands
+    if (command.privileged) {
+        const member = guild.members.get(message.author.id);
+        if (!member.roles.has(roles.exec)) {
+            let d = new Date();
+            console.log(`[${d.toLocaleString()}] Unauthorised user '${member.user.tag}' tried to use '${commandName}'`);
+            return;
+        }
+    }
+    
+    // Execute command
     try {
-        await command.execute(client, message, args);
+        await command.execute(guild, message, args);
     } catch (error) {
         console.error(error);
-        await message.reply("Sorry, an error has occurred. " +
+        await message.reply("sorry, an error has occurred. " +
             "Please try again or ping an @exec if the problem doesn't go away.");
     }
     
