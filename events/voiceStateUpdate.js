c807685567341b6dfc9025df5033f6f70dc687ab
sev@PlateSP4 <mtsev@users.noreply.github.com>
2020-04-15 15:47:32 +1000

    finish league module

--- events/voiceStateUpdate.js
+++ events/voiceStateUpdate.js
@@ -1,4 +1,5 @@
 const { server, channels } = require('../config.json');
+const { setLeague, setRegular } = require('../modules/league.js')
 
 // Export event so it can be used
 module.exports = async (client, oldMember, newMember) => {
@@ -7,10 +8,6 @@ module.exports = async (client, oldMember, newMember) => {
 
 // Toggle league mode for lounge
 async function leagueMode(oldMember, newMember) {
-
-    const league = 'League of Legends';
-    const legend = 'Lounge of Legends';
-    const regular = 'lounge';
 
     // Get guild
     const guild = oldMember.client.guilds.get(server.id);
@@ -24,54 +21,11 @@ async function leagueMode(oldMember, newMember) {
 
     // case: member joins the lounge
     if (!oldLounge && newLounge) {
-
-        // if the lounge is already league, we can ignore
-        if (lounge.name === legend) return;
-
-        // otherwise set to league mode if member is playing
-        newMember.presence.activities.forEach(async game => {
-            if (game.name === league) {
-                
-                // Set name to league
-                await lounge.setName(legend).catch(console.error);
-
-                // Open access to everyone
-                await lounge.overwritePermissions(guild.roles.get(guild.id), {
-                    CONNECT: true,
-                    SPEAK: true
-                }).catch(console.error);
-                
-                console.log(`[${new Date().toLocaleString()}] we league now`);
-                return;
-            }
-        });
+        setLeague(lounge, newMember);
     }
 
     // case: member leaves the lounge
     else if (oldLounge && !newLounge) {
-
-        // if the lounge isn't league, we can ignore
-        if (lounge.name !== legend) return;
-
-        // otherwise check if anyone is still playing
-        let stillPlaying = false;
-        lounge.members.forEach(member => {
-            member.presence.activities.forEach(game => {
-                if (game.name === league) {
-                    stillPlaying = true;
-                    return;
-                }
-            });
-
-            // already found member who is playing, no need to check more
-            if (stillPlaying) return;
-        });
-
-        // if no one is playing league anymore, revert to regular lounge
-        if (!stillPlaying) {
-            await lounge.setName(regular).catch(console.error);
-            await lounge.lockPermissions().catch(console.error);
-            console.log(`[${new Date().toLocaleString()}] back to regular lounge`);
-        }
+        setRegular(lounge);
     }
 }
