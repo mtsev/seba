c52c317972b497681e10859da3a6778da0c0cbb8
mtsev <mtsev@users.noreply.github.com>
2020-05-16 03:55:10 +1000

    more checks when setting up db

--- database/seba-schema.sql
+++ database/seba-schema.sql
@@ -7,14 +7,14 @@ Discord bot:    https://github.com/mtsev/seba
 Google script:  https://github.com/mtsev/seba-form-script
 --------------------------------------------------------------------
 Author:         Mandy Tao
-Version:        1.1
+Version:        1.2
 --------------------------------------------------------------------
 */
 
 SET NAMES utf8mb4;
 
 -- create table for form submissions
-CREATE TABLE submissions (
+CREATE TABLE IF NOT EXISTS submissions (
     submission_id   SMALLINT UNSIGNED   AUTO_INCREMENT PRIMARY KEY,
     added_on        DATETIME            DEFAULT CURRENT_TIMESTAMP,
     discord_name    VARCHAR (50)        NOT NULL,
@@ -26,7 +26,7 @@ CREATE TABLE submissions (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE utf8mb4_general_ci;
 
 -- create table to for verified members
-CREATE TABLE verified_members (
+CREATE TABLE IF NOT EXISTS verified_members (
     member_id       SMALLINT UNSIGNED   AUTO_INCREMENT PRIMARY KEY,
     discord_id      VARCHAR (20)        NOT NULL,
     submission      SMALLINT UNSIGNED   NOT NULL,
@@ -39,7 +39,7 @@ CREATE TABLE verified_members (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE utf8mb4_general_ci;
 
 -- create table to track username changes
-CREATE TABLE username_history (
+CREATE TABLE IF NOT EXISTS username_history (
     name_id         SMALLINT UNSIGNED   AUTO_INCREMENT PRIMARY KEY,
     modified        DATETIME            DEFAULT CURRENT_TIMESTAMP,
     username        VARCHAR (20)        NOT NULL,
