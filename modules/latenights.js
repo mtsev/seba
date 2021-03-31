9b8f541e4dcb9fcef0dca617c62b5d43ef9caf19
mtsev <mtsev@users.noreply.github.com>
2021-03-31 23:03:45 +1100

    latenight server pic for april fools

--- modules/latenights.js
+++ modules/latenights.js
@@ -27,7 +27,7 @@ function show(guild) {
     // Make cron job to set up channels and return job for main to run
     return new CronJob(`00 ${i} ${H} * * *`, async () => {
         // Edit the guild icon
-        await guild.setIcon('./static/cursed_logo.png').catch(console.error);
+        await guild.setIcon('./static/april_night.png').catch(console.error);
 
         // Add wenkiss emoji
         await guild.emojis.create('./static/wenkiss.png', 'wenkiss').catch(console.error);
