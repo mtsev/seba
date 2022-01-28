aa5c55b2bddf3d0693674d44dcd2993287d5bdc5
mtsev <mtsev@users.noreply.github.com>
2022-01-29 00:25:02 +1100

    modify move command to only accept dest vc as arg, allowing whitespace

--- commands/move.js
+++ commands/move.js
@@ -3,52 +3,17 @@ module.exports = {
     name:        'move',
     description: 'Move all members from one voice channel to another. ' +
                  'Can be used in any channel.',
-    usage:      '[from_channel] <to_channel>',
-    privileged: true,
+    usage:      '<dest_channel>',
+    privileged: false,
     execute:    execute
 };
 
 // Actual command to execute
 async function execute(guild, message, args) {
     let botReply;
-    let fromChannel;
-    let toChannel;
 
-    // Ignore DMs
-    if (message.channel.type !== 'text') return;
-
-    // If only one argument supplied, move from user's current channel
-    if (args.length === 1) {
-        fromChannel = guild.member(message).voice.channel;
-        toChannel = guild.channels.cache
-            .find(channel => channel.name.toLowerCase() === args[0].toLowerCase());
-
-        // Check if channels exist
-        if (!fromChannel) {
-            botReply = 'Please join voice channel to move from or specify channel:' +
-                        '```!move <from_channel> <to_channel>```';
-        } else if (!toChannel) {
-            botReply = `Couldn't find channel \`${args[0]}\``;
-        }
-    }
-
-    // If two arguments supplied, move from arg1 to arg2
-    else if (args.length === 2) {
-        fromChannel = guild.channels.cache
-            .find(channel => channel.name.toLowerCase() === args[0].toLowerCase());
-        toChannel = guild.channels.cache
-            .find(channel => channel.name.toLowerCase() === args[1].toLowerCase());
-
-        // Check if channels exist
-        if (!fromChannel) {
-            botReply = `Couldn't find channel \`${args[0]}\``;
-        } else if (!toChannel) {
-            botReply = `Couldn't find channel \`${args[1]}\``;
-        }
-    }
-
-    // Incorrect arguments supplied
-    else {
+    // Command must take the destination channel as an argument.
+    if (args.length === 0) {
         botReply = `usage: ${message.client.prefix}${module.exports.name} ` +
                    `${module.exports.usage}`;
         const msg = await message.reply('```' + botReply + '```').catch(console.error);
@@ -56,22 +21,22 @@ async function execute(guild, message, args) {
         return;
     }
 
-    // If we have a bot reply already then that means something's gone wrong
-    if (botReply) {
-        await message.channel.send(botReply).catch(console.error);
-        return;
+    // Source is user's current voice channel.
+    const fromChannel = guild.member(message).voice.channel;
+    const toChannel = guild.channels.cache
+        .find(channel => channel.type !== 'voice' &&
+            channel.name.toLowerCase().contains(args.join(' ').toLowerCase()));
+
+    // Check that both source and destination voice channels exist
+    if (!fromChannel) {
+        botReply = 'Please join voice channel to move from.';
+    } else if (!toChannel) {
+        botReply = `Couldn't find voice channel \`${args.join(' ')}\``;
     }
 
-    // Check that both channels are actually voice channels
-    if (fromChannel.type !== 'voice') {
-        botReply = `\`${fromChannel.name}\` is not a voice channel, please try again`;
-    } else if (toChannel.type !== 'voice') {
-        botReply = `\`${toChannel.name}\` is not a voice channel, please try again`;
-    }
-
-    // Good to go!
+    // Otherwise, good to go! Let's move everyone.
     else {
-        // Move everyone
+        // Beware the rate limit. Hasn't been an issue recently but was before.
         for (const member of fromChannel.members.values()) {
             try {
                 await member.voice.setChannel(toChannel);
@@ -83,7 +48,6 @@ async function execute(guild, message, args) {
             }
         }
 
-        // Set output message
         botReply = `Moved everyone from \`${fromChannel.name}\` ` +
                     `to \`${toChannel.name}\``;
     }
